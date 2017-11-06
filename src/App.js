d7ad11df7b2faa003d4509c180b2d5f23927ab0b
Alexia Young <ayoung@preparedhealth.com>
2017-11-06 12:08:22 -0600

    trying to incorporate react sticky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import css from './App.css';
+import { StickyContainer, Sticky } from 'react-sticky';
+import { Container, Row, Col } from 'reactstrap';
+import { CSSTransitionGroup } from 'react-transition-group';
 import Navbar from './components/navbar/navbar.jsx';
 import Banner from './components/banner/banner.jsx';
 import ProfileCard from './components/profile-card/profile-card.jsx';
@@ -7,17 +10,24 @@ import FeedCard from './components/feed-card/feed-card.jsx';
 
 const fakeData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
       <div>
         <Navbar />
         <div className={css.appContent}>
           <Banner />
-          <div className={css.profileSection}>
-            <ProfileCard />
-          </div>
-          <div className={css.feedSection}>{fakeData.map(() => <FeedCard />)}</div>
+          <StickyContainer style={{ height: 1200 }}>
+            <div className={css.profileSection}>
+              <Sticky topOffset={-50}>
+                {({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
+                  console.log('sticky: ', isSticky, wasSticky, style);
+                  return <ProfileCard style={style} />;
+                }}
+              </Sticky>
+            </div>
+            <div className={css.feedSection}>{fakeData.map(() => <FeedCard />)}</div>
+          </StickyContainer>
         </div>
       </div>
     );
